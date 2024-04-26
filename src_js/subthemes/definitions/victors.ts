06eddf75223d130ccd1771233b0063290af6a8d9
HugoKim36 <160697135+HugoKim36@users.noreply.github.com>
2024-04-25 21:30:21 -0400

    Create victors.ts

---
+++ src_js/subthemes/definitions/victors.ts
@@ -0,0 +1,13 @@
+import { SubthemeDefinitionType } from '../Subtheme';
+import common_dark_theme_colors from './common_dark_theme_colors';
+
+const victors_modern_theme_vars: SubthemeDefinitionType = {
+  light: {
+  
+  },
+  dark: {
+    
+  },
+};
+
+export default victors_modern_theme_vars;
